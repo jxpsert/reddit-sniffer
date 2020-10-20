08f112ca8e0856a23c004fc9f6223fc76b410308
jxpsert <jasperdgp@outlook.com>
2020-10-20 16:48:40 +0200

    Renamed methods, added a safety check and moved refresh from hardcode to config.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Using;
 - [@discordjs/collection](https://www.npmjs.com/package/@discordjs/collection)
 - [rss-feed-emitter](https://www.npmjs.com/package/rss-feed-emitter)
 - [html-entities](https://www.npmjs.com/package/html-entities)
-- [chalk](https://www.npmjs.com/package/@discordjs/collection) (CLI version)
-- [beepbeep](https://www.npmjs.com/package/beepbeep) (CLI version)
+- [chalk](https://www.npmjs.com/package/@discordjs/collection) (CLI)
+- [beepbeep](https://www.npmjs.com/package/beepbeep) (CLI)
 
 (This might be very inefficient, but I don't care because it works. I made this for myself.)
--- reddit.js
+++ reddit.js
@@ -9,8 +9,8 @@ const entities = new Entities();
 class RedditClient extends EventEmitter {
     constructor(data) {
         super();
-        this.options = data;
-        this.posts = new Collection();
+        this.options = data; // Currently just the refresh
+        this.posts = new Collection(); // Thank you, @discord.js/collection!
         config.subreddits.forEach(subreddit => {
             feeder.add({
                 url: `http://www.reddit.com/r/${subreddit}/new/.rss?sort=new`,
@@ -19,32 +19,33 @@ class RedditClient extends EventEmitter {
             });
         });
         feeder.on('newpost', data => {
-            if (!data.title) return;
-            let PostData = new Submission({
+            if (!data.title) return; // Not sure why, but sometimes the title is undefined
+            let SubmissionData = new Submission({
                 title: data.title,
-                id: data.guid,
-                summary: parseDesc(data.description),
-                description: data.description.substring(0, data.description.indexOf("submitted by")),
-                author: data.author,
-                subreddit: data.categories[0],
+                id: data.guid, // Unique identifier
+                summary: parseDesc(data.description), // CLI
+                description: entities.decode(data.description.substring(0, data.description.indexOf("submitted by"))), // RSS feed supplies HTML, and includes the 'submitted by part'
+                author: data.author, // This is turned into a User in the Submission constructor
+                subreddit: data.categories[0], // categories is an Array for whatever reason, but it has only 1 entry, thus [0]
                 image: data.image,
-                url: data.link.replace('www', 'new'),
+                url: data.link.replace('www', 'new'), // For new Reddit UI
                 date: data.pubdate
             });
-            this.newPost(PostData);
+            this.newSubmission(SubmissionData);
         })
     }
 
-    newPost(data) {
+    newSubmission(data) { // Adding the new Submission to this.posts
         if (!data) return new Error("Shit has hit the fan");
+        if(this.posts.get(data.id)) return; // Check if the same post has already been shown. This happens.
         this.posts.set(data.id, data);
         return this.emit('post', data);
     }
 }
 
-function parseDesc(data){
+function parseDesc(data){ // For CLI support (Submission.summary)
     let newstring = data.replace(/<[^>]*>?/gm, '')
-    .replace(data.title, ' ')
+    .replace(data.title, '')
     .substring(0, data.indexOf("submitted by"))
     .substring(0, 197) + "...";
     newstring = entities.decode(newstring);
